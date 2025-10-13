3d295005fbd2f652f9f221703ce227b4fa2dedb9
haithanh019 <haithanh019@gmail.com>
2025-10-14 01:30:05 +0700

    [ThanhHH - 2025-10-14] Fix Issues Deploy

--- Flower_Shop/FlowerShop_WebApp/Program.cs
+++ Flower_Shop/FlowerShop_WebApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using System.Net.Http.Headers;
+using System.Text.Json;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Localization;
 
@@ -53,7 +54,12 @@ namespace FlowerShop_WebApp
                     }
                 );
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder
+                .Services.AddControllersWithViews()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                });
             builder.Services.AddHttpContextAccessor();
 
             var app = builder.Build();
